93c739aa2a33422737de09c9576c79ccae6af3f7
jordanvargom <jordanvargom@gmail.com>
2023-04-05 21:06:43 -0300

    ;)

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -1,5 +1,4 @@
 import style from "./Presentation.module.css";
-import TypeWriterEffect from "react-typewriter-effect";
 import { useNavigate } from "react-router-dom";
 function Presentation() {
   const navigte = useNavigate();
@@ -7,20 +6,7 @@ function Presentation() {
     <div className={style.container}>
       <img src="/Home-2.jpg" alt="Home" className={style.image} />
       <div className={style.textContainer}>
-        <TypeWriterEffect
-          textStyle={{
-            fontFamily: "Red Hat Display",
-            color: "#e1e1e1",
-            fontWeight: 1000,
-            fontSize: "3em",
-          }}
-          startDelay={2000}
-          cursorColor="#3F3D56"
-          multiText={["The Rick and Morty Wiki"]}
-          multiTextLoop={true}
-          multiTextDelay={1000}
-          typeSpeed={200}
-        />
+        <h1>The Rick and Morty Wiki</h1>
         <p className={style.description}>
           If you are a fan of Rick and Morty, then you are going to love this
           app. With its sleek design and user-friendly interface, you'll have
